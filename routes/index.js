43e2b88342f7597186277f4e07e24604eb2b2da2
RavishGupta <96822160+RavishGupta@users.noreply.github.com>
2022-10-19 15:05:43 +0530

    Update index.js
    created some new routes for the digital outpass

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,81 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
+const app = express();
+const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
+const db = require('./config/mongoose');
+// used for session cookie
+const session = require('express-session');
+const passportLocal = require('./config/passport-local-strategy');
+const passport = require('./config/passport-local-strategy');
+const MongoStore = require('connect-mongo');
+const sassMiddleware=require('node-sass-middleware');
+const customMware = require('./config/middleware');
+const flash = require('connect-flash');
 
-const router = express.Router();
-const homeController = require('../controllers/home_controller');
 
-console.log('router loaded');
+app.use(sassMiddleware({
+    src:'./assets/scss',
+    dest:'./assets/css',
+    debug:true,
+    outputStyle: 'expanded',
+    prefix:"/css"
+
+}));
+app.use(express.urlencoded());  
+
+app.use(cookieParser());
+
+app.use(express.static('./assets'));
+
+app.use(expressLayouts);
+// extract style and scripts from sub pages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 
 
-router.get('/', homeController.home);
-router.use('/users', require('./users'));
-
-// for any further routes, access from here
-// router.use('/routerName', require('./routerfile));
 
 
-module.exports = router;
+
+// set up the view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+// mongo store is used to store the sesion cookie
+app.use(session({
+    name:'codeial',
+    // todo change the secret before deploymwnt in production mode
+    secret: 'blahsomething',
+    saveUninitialized:false,
+    resave:false,
+    cookie:{
+        maxAge: (1000 * 60 * 100)
+    },
+    store:  MongoStore.create({
+        
+            mongoUrl:'mongodb://localhost/codeial_development',
+            autoRemove:'disabled'
+    },function(err){
+        console.log(err || 'connect mongo-db setup ok');
+    })
+
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(passport.setAuthenticatedUser);
+
+app.use(flash());
+app.use(customMware.setFlash);
+
+// use express router
+app.use('/', require('./routes'));
+
+app.listen(port, function(err){
+    if (err){
+        console.log(`Error in running the server: ${err}`);
+    }
+
+    console.log(`Server is running on port: ${port}`);
+});
